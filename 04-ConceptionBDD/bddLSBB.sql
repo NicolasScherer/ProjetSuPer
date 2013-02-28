8bfe05fe4b89196d92d770f440e34bbb48616cc5
BriceJamin <brice.jamin@hotmail.fr>
2013-02-28 14:33:55 +0100

    Ajout identifiants pour BDD locale

--- 04-ConceptionBDD/bddLSBB.sql
+++ 04-ConceptionBDD/bddLSBB.sql
@@ -1,32 +1,37 @@
+-- Nom de la bdd : bdd_super
+-- Utilisateur de la bdd : user_super
+-- Mot de passe de l'utilisateur : mdp_super
+
 -- DROP DATABASE IF EXISTS bdd_super;
 
--- CREATE DATABASE IF NOT EXISTS bdd_super CHARACTER SET 'utf8';
+CREATE DATABASE IF NOT EXISTS bdd_super CHARACTER SET 'utf8' COLLATE utf8_bin;
 
 USE bdd_super;
-
-DROP TABLE IF EXISTS lecteur;
-
-CREATE TABLE lecteur
-(
-   num_lecteur 	INT UNSIGNED 		NOT NULL,
-   num_lieu 	INT UNSIGNED UNIQUE	NOT NULL,
-   ip 		VARCHAR (15) UNIQUE	NOT NULL,
-   estConnecte 	TINYINT UNSIGNED	NOT NULL,
-   
-   PRIMARY KEY (num_lecteur)
-   
-)ENGINE=INNODB;
-
 
 DROP TABLE IF EXISTS lieu;
 
 CREATE TABLE lieu
 (
-   num_lieu	INT UNSIGNED 	NOT NULL,
-   legende	VARCHAR(50) 	NOT NULL,
-   
+   num_lieu	INT UNSIGNED	NOT NULL,
+   legende	VARCHAR(100)	NOT NULL,
+
    PRIMARY KEY (num_lieu, legende)
-   
+
+)ENGINE=INNODB;
+
+
+DROP TABLE IF EXISTS lecteur;
+
+CREATE TABLE lecteur
+(
+   num_lecteur	INT UNSIGNED		NOT NULL,
+   num_lieu	INT UNSIGNED UNIQUE	NOT NULL,
+   ip		VARCHAR (15) UNIQUE	NOT NULL,
+   estConnecte	TINYINT UNSIGNED	NOT NULL,
+   constraint estConnecte_is_bool	check(estConnecte BETWEEN 0 and 1),
+
+   PRIMARY KEY (num_lecteur)
+
 )ENGINE=INNODB;
 
 
